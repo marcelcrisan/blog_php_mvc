6624099740fac13ff8b462f1d54e7ea111ff6ffe
marcel crisan <marcelcrisan@outlook.com>
2019-06-21 11:02:52 +0200

    new function in controller/frontend.php

--- controller/frontend.php
+++ controller/frontend.php
@@ -14,3 +14,15 @@
 
         require('./view/frontend/postView.php');
     }
+
+    function addComment($postId, $author, $comment)
+    {
+        $affectedLines = postComment($postId, $author, $comment);
+
+        if($affectedLines === false) {
+            die('Impossible d\'ajouter le commentaire !');
+        }
+        else {
+            header('Location: index.php?action=post&id=' . $postId);
+        }
+    }
