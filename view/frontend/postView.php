085f2041d249b33afa1949b44710a18233d387df
marcel crisan <marcelcrisan@outlook.com>
2019-06-21 15:01:40 +0200

    update postView.php for comments

--- view/frontend/postView.php
+++ view/frontend/postView.php
@@ -16,6 +16,19 @@
         </div>
 
         <h2>Commentaires</h2>
+        <form action="index.php?action=addComment&amp;id=<?= $post['id'] ?>" method="post">
+            <div>
+                <label for="author">Auteur</label><br>
+                <input type="text" id="author" name="author">
+            </div>
+            <div>
+                <label for="comment">Commentaire</label><br>
+                <textarea name="comment" id="comment" cols="30" rows="10"></textarea>
+            </div>
+            <div>
+                <input type="submit">
+            </div>
+        </form>
 
         <?php
             while ($comment = $comments->fetch())
